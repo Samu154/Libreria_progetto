ca5510b0d87f6b937769eb4a1d4b449c1613028d
Samu154 <samuele.bertin@studenti.unipd.it>
2025-09-05 11:37:46 +0200

    interfaccia

--- main.cpp
+++ main.cpp
@@ -1,28 +1,15 @@
 #include <QApplication>
 #include <QDebug>
-#include <QJsonDocument>
-#include "Book.h"
-#include "Film.h"
-#include "Article.h"
 #include "MainWindow.h"
-#include <QApplication>
 
-int main(int argc, char** argv) {
+int main(int argc, char *argv[])
+{
     QApplication app(argc, argv);
 
-    Book b("Il nome della rosa", "Umberto Eco", 1980);
-    qDebug() << "Titolo:" << b.getTitle();
-    qDebug() << "Summary:" << b.getSummary();
+    qDebug() << "Avvio applicazione GUI";
 
-    // serializza in json e stampa
-    QJsonObject j = b.toJson();
-    QJsonDocument doc(j);
-    qDebug() << "JSON compatto:" << doc.toJson(QJsonDocument::Compact);
+    MainWindow w;
+    w.show();
 
-    // test della azione polimorfica
-    b.performAction();
-
-    // Non avviamo l'event loop (app.exec()) perché questo main è
-    // solo un test console. Se poi hai GUI, usa app.exec().
-    return 0;
+    return app.exec();
 }
